a0b8829536891d1d9f338b9bdc0e1e6972bd5e1a
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-12-30 13:30:23 +0530

    create AppInitializer file

---
+++ src/main/AppInitializer.java
@@ -0,0 +1,15 @@
+package main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+        ctx.registerShutdownHook();
+
+         Object bean = ctx.getBean();
+    }
+
+}
