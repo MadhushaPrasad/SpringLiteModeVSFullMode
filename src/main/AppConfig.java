b11371062690fd810f1a173f24596ebd7522ec24
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-12-30 13:27:44 +0530

    create AppConfig file

---
+++ src/main/AppConfig.java
@@ -0,0 +1,10 @@
+package main;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages ="bean")
+public class AppConfig {
+
+}
